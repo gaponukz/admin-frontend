3e8efdb4bfd3d41ffae0611822e7e5c9a6ac4279
gaponukz <gaponukz54@gmail.com>
2022-05-09 11:38:40 -0700

    Update

--- src/components/MessagesPage.js
+++ src/components/MessagesPage.js
@@ -3,18 +3,27 @@ import { useState, useEffect } from 'react'
 
 const MessagesPage = props => {
     const [messagesList, setMessagesList] = useState([])
-    const [isLoginSuccessful, setLoginSuccess] = useState(false)
+
+    const removeMessageImage = "https://img.icons8.com/windows/344/delete-forever.png"
+    const buttonStyle = { background: "none", border: "none" }
+
+    const removeMessage = async message => {
+        await fetch(`${props.apiServer}/remove_message?adminApiKey=${props.adminApiKey}&_id=${message._id}`)
+        .then(async response => await response.json()).then(async response => {
+            if (response.deletedCount === 1) {
+                setMessagesList(messagesList.filter(item => item._id !== message._id))
+            }
+        })
+    }
 
     useEffect(() => {try {
         fetch(`${props.apiServer}/get_messages?adminApiKey=${props.adminApiKey}`)
         .then(response => response.json()).then(response => {
-            setLoginSuccess(response.isLoginSuccess)
             localStorage.setItem('isAuth', 'true')
             setMessagesList(response.messages)
         })
     } catch (error) {
         console.error(error)
-        setLoginSuccess(false)
         setMessagesList([])
     }}, [])
 
@@ -27,6 +36,11 @@ const MessagesPage = props => {
                         <td>{message.message}</td>
                         <td>{message.date}</td>
                         <td>{message.gmail}</td>
+                        <td>
+                            <button style={buttonStyle} onClick={()=>removeMessage(message)}>
+                                <img src={removeMessageImage} style={{width: "25px"}}/>
+                            </button>
+                        </td>
                     </tr>
                 )}
             </tbody>
